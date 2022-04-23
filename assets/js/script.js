0a73cc93f188a447cfd6e4704c8322283c0cd861
HollosT <hollos.tamas@ravensoft.hu>
2022-04-23 11:59:55 +0200

    new content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,38 @@
 
-// Fetching the data from WP
-fetch("https://marekfurik.com/wp-json/wp/v2/posts")
+function getPosts() {
+    fetch("https://marekfurik.com/wp-json/wp/v2/posts?categories=8")
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          for (let i = 0; i < data.length; i++) {
-            const section = `
-            <section>
-                <h2></h2>
-                <a href="">
-                <h4></h4>
-                    <img src="" alt="">
-                    <p></p>
-                </a>
-            </section>
-        `;
-        
-        console.log(section);
-        return section;
-    };
-    addHTML('main', section);
+    getRecipeData(data)
+    
 });
+}
+getPosts();
+function getRecipeData(data) {
+    for(let i = 0; i <data.length; i++){
+        let dataOfRecipes = data[i].acf;
+        console.log(dataOfRecipes);
+        drawHomePage(dataOfRecipes)
+        return dataOfRecipes;
+    }
+}
+
+function drawHomePage(data) {
+    let section = `
+        <section>
+        <h2>${data.introduction.name}</h2>
+        <a href="">
+        <h4></h4>
+            <img src="" alt="">
+            <p></p>
+        </a>
+    </section>
+`;
+drawHtml('main', section);
+}
 
 //Adding content to the Html
-
 function drawHtml(selector, newContent) {
     document.querySelector(selector).innerHTML += newContent;
 }
-
-// const addHTML = (selector, content) => {
-//     const elements = document.querySelectorAll(selector);
-//     elements.forEach(element => {
-//       element.innerHTML += content;
-//     });
-//   };
