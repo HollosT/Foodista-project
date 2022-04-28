2d787730430a0c941961376ea96be4e10dabce01
Marek Furik <marekfurik@post.cz>
2022-04-28 10:19:00 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function drawInternationalSection(data) {
     let cardInt = `
         <article>
             <a href="?pageId=${data[i].id}">
-                <img class="recipe-img" src="${data[i].acf.introduction.image}" alt="">
+                <img class="recipe-img" src="${data[i].acf.introduction.image}" alt="" class="teaser-img">
                 <h4>${data[i].acf.introduction.name}</h4>
                 <p>${data[i].acf.introduction.teaser}</p>
             </a>
